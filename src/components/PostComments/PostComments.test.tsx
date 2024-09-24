18983c58183ec16125290e2382cee05c60b25b33
Bruno Campos <brunosopmac@gmail.com>
2024-09-24 14:29:27 -0300

    Conclusão do exercício

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+  test('Deve renderizar o componente corretamente', () => {
+    render(<PostComment/>);
+    expect(screen.getByText('Comentar')).toBeInTheDocument();
+  });
+    
+  test('Deve renderizar o componente com dois comentarios', () => {
+    render(<PostComment />);
+    
+    const commentInput = screen.getByTestId('comment-input');
+    const submitButton = screen.getByText('Comentar');
+    
+    fireEvent.change(commentInput, { target: { value: 'Primeiro comentário' } });
+    fireEvent.click(submitButton);
+    
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+    
+    fireEvent.change(commentInput, { target: { value: 'Segundo comentário' } });
+    fireEvent.click(submitButton);
+    
+    expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
